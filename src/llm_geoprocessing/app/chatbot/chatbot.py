5f7dd01b7cc62ade08f20a9ab84655d40d447bc0
Escobares-Cristhian Notebook CONAE 027149 <cristhian.escobares@ig.edu.ar>
2025-11-05 15:44:35 -0300

    Small add: models rate limits in Chatbot class v2

--- src/llm_geoprocessing/app/chatbot/chatbot.py
+++ src/llm_geoprocessing/app/chatbot/chatbot.py
@@ -6,7 +6,7 @@ from llm_geoprocessing.app.llm.LLM import Gemini, ChatMemory
 class Chatbot:
     def __init__(self):
         # Gemini
-        # self.chat = Gemini(model="gemini-2.5-pro", quiet=True)          # 125.000 TokensPD but have free rate limit of 2 per minute, so it is useless for interactive chat
+        # self.chat = Gemini(model="gemini-2.5-pro", quiet=True)        # 125.000 TokensPD but have free rate limit of 2 per minute, so it is useless for interactive chat
         self.chat = Gemini(model="gemini-2.5-flash", quiet=True)      # 250 RPD
         # self.chat = Gemini(model="gemini-2.0-flash", quiet=True)      # 250 RPD
         # self.chat = Gemini(model="gemini-2.0-flash-lite", quiet=True) # 200 RPD
