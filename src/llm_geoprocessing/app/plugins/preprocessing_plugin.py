dff26f1fd2fd9d99df1ce2b2c249940777d6947c
Escobares-Cristhian Notebook CONAE 027149 <cristhian.escobares@ig.edu.ar>
2025-11-06 14:14:47 -0300

    Small fix on GEE preprocessing plugin documentation

--- src/llm_geoprocessing/app/plugins/preprocessing_plugin.py
+++ src/llm_geoprocessing/app/plugins/preprocessing_plugin.py
@@ -14,6 +14,10 @@ def get_documentation_preprocessing() -> str:
     return (
         "Preprocessing (GEE):\n"
         "- Use the listed satellites/sensors directly with the geoprocesses.\n"
-        "- Choose bands that exist in the selected product (e.g., S2: B2,B3,B4; Landsat 8: SR_B2,SR_B3,SR_B4).\n"
+        "- Choose bands that exist in the selected product:"
+        "    - Sentinal-2: B1, B2, B3, B4, B5, B6, B7, B8, B8A, B9, B10, B11, B12\n"
+        "    - Landsat 8/9: SR_B1, SR_B2, SR_B3, SR_B4, SR_B5, SR_B6, SR_B7, SR_QA_AEROSOL, ST_B10, ST_ATRAN, ST_CDIST, ST_DRAD, ST_EMIS, ST_EMSD, ST_QA, ST_TRAD, ST_URAD, QA_PIXEL, QA_RADSAT\n"
+        "    - Landsat 5: SR_B1, SR_B2, SR_B3, SR_B4, SR_B5, SR_B6, SR_B7\n"
+        "    - MODIS: sur_refl_b01, sur_refl_b02, sur_refl_b03, sur_refl_b04, sur_refl_b05, sur_refl_b06, sur_refl_b07\n"        
         "- Provide bbox (lon/lat) and dates; no additional preprocessing is required here.\n"
     )
