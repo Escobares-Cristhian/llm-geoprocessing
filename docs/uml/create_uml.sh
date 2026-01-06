0b8bd41df40f5f2a27f07f9d07e944230b600d6e
Escobares-Cristhian Notebook CONAE 027149 <cristhian.escobares@ig.edu.ar>
2026-01-06 13:30:56 -0300

    readme: Fix UML class with missing classes

--- docs/uml/create_uml.sh
+++ docs/uml/create_uml.sh
@@ -1,6 +1,11 @@
 mkdir -p docs/uml
 
 PYTHONPATH=src python3 -c "import llm_geoprocessing; print(llm_geoprocessing.__file__)"
-echo "PYTHONPATH=$PYTHONPATH"
+PYTHONPATH=src python3 -c "import cli; print(cli.__file__)"
+export PYTHONPATH=src
 
-PYTHONPATH=src pyreverse --verbose --source-roots src --max-depth 50 -o puml -d docs/uml -p llm_geoprocessing -A -S -m y --ignore=tests,docs,docker,secrets,utils,tmp,gee_out llm_geoprocessing
+pyreverse --verbose --source-roots src --max-depth 50 \
+  -o puml -d docs/uml -p llm_geoprocessing -A -S -m y \
+  --ignore=tests,docs,docker,secrets,utils,tmp,gee_out \
+  llm_geoprocessing cli
+
